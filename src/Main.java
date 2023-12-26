b409151eab2a5c8c212133b62b2409261d0ac04f
ManuOmulo <omuloemmanuel@gmail.com>
2023-12-26 23:27:07 +0300

    Refactored code in main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 public class Main {
   public static void main(String[] args) {
-    var order1 = new Order();
-    order1.getReceipt();
+    makeOrder();
 
-    var burger = new Burger("deluxe");
-    var drink = new Drink("sprite", "grande");
-    var side = new SideItem("coleslaw");
+    makeOrder("deluxe", "sprite", "grande", "coleslaw");
+
+    makeOrder("chicken", "fanta", "large", "icecream");
+  }
+
+  public static void makeOrder(String burgerType, String drinkType, String drinkSize, String sideType) {
+    var burger = new Burger(burgerType);
+    var drink = new Drink(drinkType, drinkSize);
+    var side = new SideItem(sideType);
     var order2 = new Order(burger, drink, side);
     order2.getReceipt();
+  }
+
+  public static void makeOrder() {
+    var order1 = new Order();
+    order1.getReceipt();
   }
 }
